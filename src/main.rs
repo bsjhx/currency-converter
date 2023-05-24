7e19920b86e0a0d2c24bb834a00055404c95afe8
bsjhx <rakoczymichal@hotmail.com>
2023-05-24 21:46:45 +0200

    Added argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    currency_in: Option<String>,
+    currency_out: Option<String>,
+    amount: Option<u32>
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    if let Some(currency_in) = args.currency_in {
+        println!("Value: {:}", currency_in);
+    } else {
+        println!("Empty");
+    }
 }
