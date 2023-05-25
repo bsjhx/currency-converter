fbeb08c9cdad094fbedb867888e6fc0667ec45fc
bsjhx <rakoczymichal@hotmail.com>
2023-05-25 18:04:17 +0200

    Added basic API call to freecurrencyapi.com

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,22 @@ struct Args {
     amount: Option<u32>
 }
 
-
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
     if let Some(currency_in) = args.currency_in {
         println!("Value: {:}", currency_in);
     } else {
         println!("Empty");
     }
+    let params = [("apikey", "")];
+    let url = reqwest::Url::parse_with_params("https://api.freecurrencyapi.com/v1/status", &params)?;
+    let body = reqwest::get(url.as_str())
+        .await?
+        .text()
+        .await?;
+
+    println!("body = {:?}", body);
+
+    Ok(())
 }
